c9fe7034dc4d66b319995240df7b7c81bff3e76e
sachdevaansh84 <117078192+sachdevaansh84@users.noreply.github.com>
2022-11-24 21:27:52 +0530

    Add files via upload

---
+++ star pattern.py
@@ -0,0 +1,10 @@
+#for increment
+a=int(input())  
+for i in range(1,a+1):
+    for j in range(1,i+1):
+        print("*",end=" ")
+    print()    
+
+#for decrement
+#i range(a,0,-1), j range(1,i+1)
+    #print("")
